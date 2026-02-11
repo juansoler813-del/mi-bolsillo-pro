b0dedc8ca502067985464dca45bebb144f4e0bcf
juansoler813-del <juansoler813@gmail.com>
2026-02-11 16:55:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import flet as ft
 
-app = ft.app(target=lambda: None)
-
 def main(page: ft.Page):
     page.title = "Mi Bolsillo Pro"
     page.theme_mode = "dark"
     page.bgcolor = "#0f172a"
-    page.add(ft.Text("SI ESTO FUNCIONA, YA CASI", size=30, color="white"))
+    page.add(ft.Text("¡FUNCIONA!", size=40, color="white"))
 
-if __name__ == "__main__":
-    ft.app(target=main)
+app = ft.app(target=main)
